129c9c6afaee2c19bf690269292cc56523619c05
Marty Sloan <marty@karmatek.io>
2019-12-18 16:44:46 -0500

    add reactstrap to favorites list

--- src/components/Favorite/FavoriteList.js
+++ src/components/Favorite/FavoriteList.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Favorite from './Favorite'
 
+// reactstrap components
+import { Col, ListGroup, ListGroupItem, Row } from 'reactstrap'
+
 const FavoriteList = () => {
   const favorites = useSelector(state => state.favorites)
 
@@ -10,16 +13,16 @@ const FavoriteList = () => {
       <Favorite favoriteId={favorite.id} key={favorite.id} />
     ))
   ) : (
-    <div className='row pb-3'>
-      <div className='col-md-8 offset-md-2 text-center'>
-        <ul className='list-group'>
-          <li className='list-group-item disabled' aria-disabled='true'>
+    <Row>
+      <Col sm={12} md={{ size: 8, offset: 2 }} className='text-center'>
+        <ListGroup>
+          <ListGroupItem className='disabled' aria-disabled='true'>
             No Favorites Yet! <br />
             Start sending queries, then add them to your favorites list.
-          </li>
-        </ul>
-      </div>
-    </div>
+          </ListGroupItem>
+        </ListGroup>
+      </Col>
+    </Row>
   )
 }
 
