3ce6a050c1bb929de51817d654bd5b92106768cb
Martin Sloan <marty@karmatek.io>
2019-12-20 13:35:19 -0500

    remove css folder from repo

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ import { createBrowserHistory } from 'history'
 import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'assets/scss/now-ui-dashboard.scss?v1.2.0'
-import 'assets/css/demo.css'
 import './styles/custom.css'
 import './store/actions/updateActions'
 
